eda7dcc7f06444bfd7001b09b8eef3d2174eca14
Rodolfo Ramirez <rodormz98@gmail.com>
2025-09-18 20:25:38 -0600

    Initial Commit

---
+++ SurivuTests/SurivuTests.swift
@@ -0,0 +1,16 @@
+//
+//  SurivuTests.swift
+//  SurivuTests
+//
+//  Created by Rodolfo Ramirez on 18/09/25.
+//
+
+import Testing
+
+struct SurivuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
