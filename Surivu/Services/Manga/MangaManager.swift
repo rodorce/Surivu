6e042128f0618a30dd1c9210dfa62f99e86c553a
Rodolfo Ramirez <rodormz98@gmail.com>
2025-09-26 13:16:20 -0600

    [Formatting] Small formatting in MangaManager

--- Surivu/Services/Manga/MangaManager.swift
+++ Surivu/Services/Manga/MangaManager.swift
@@ -26,13 +26,30 @@ class MangaManager {
         return mangas.first(where: {$0.id == id})
     }
     
+    //MARK: - Chapters
+    func getChapters(mangaId: String, limit: Int?, offset: Int) async throws -> [ChapterDetail] {
+        let chapterEntities: [ChapterEntity] = try await service.getChapters(mangaId: mangaId, limit: limit, offset: offset)
+        return chapterEntities.map { entity in
+            ChapterDetail(chapterEntity: entity)
+        }
+    }
+    
+    func getChapterImages(chapterId: String) async throws -> [String] {
+        return try await service.getChapterImages(chapterId: chapterId)
+    }
+    
+    //MARK: - Helper Functions
     private func generateMangaDetails(mangaEntities: [MangaEntity]) async throws -> [MangaDetail] {
         return try await withThrowingTaskGroup(of: MangaDetail.self) { group in
             for mangaEntity in mangaEntities {
                 group.addTask {
                     let coverArtId = mangaEntity.relationships.first(where: {$0.type == .coverArt})?.id ?? ""
                     let coverEntity: CoverEntity = try await self.service.getCover(coverArtId: coverArtId)
-                    let coverUrl = try await URLRequest(endpoint: CoversImageEndpoint(path: "covers/\(mangaEntity.id)/\(coverEntity.attributes.fileName)")).url?.absoluteString
+                    let coverUrl = try await URLRequest(
+                        endpoint: CoversImageEndpoint(
+                            path: "covers/\(mangaEntity.id)/\(coverEntity.attributes.fileName)"
+                        )
+                    ).url?.absoluteString
                     return await MangaDetail(
                         id: mangaEntity.id,
                         coverId: coverEntity.id,
@@ -51,17 +68,5 @@ class MangaManager {
             }
             return details
         }
-    }
-    
-    //MARK: - Chapters
-    func getChapters(mangaId: String, limit: Int?, offset: Int) async throws -> [ChapterDetail] {
-        let chapterEntities: [ChapterEntity] = try await service.getChapters(mangaId: mangaId, limit: limit, offset: offset)
-        return chapterEntities.map { entity in
-            ChapterDetail(chapterEntity: entity)
-        }
-    }
-    
-    func getChapterImages(chapterId: String) async throws -> [String] {
-        return try await service.getChapterImages(chapterId: chapterId)
     }
 }
